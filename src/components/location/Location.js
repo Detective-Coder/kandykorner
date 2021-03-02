d2bf0f2bf07eaa9bdfed01bf5a332cd3421e4992
Detective-Coder <aaronfrankenfield@gmail.com>
2021-03-02 09:59:29 -0500

    fixed the error in Location.js

--- src/components/location/Location.js
+++ src/components/location/Location.js
@@ -1,7 +1,8 @@
 import React from "react"
 
-export const Location = ({location}) => {
+export const Location = ({location}) => (
+  
   <section className="location">
     <h3 className="location__address">{location.address}</h3>
   </section>
-}
+)
